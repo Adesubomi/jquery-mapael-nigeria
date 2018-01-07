5726dfed81b3c9d458038f1c992dc3f5ab073633
Oniya Daniel <daniel.prince84@gmail.com>
2018-01-07 23:50:18 +0100

    created sample map of nigeria

---
+++ scripts/major-city-plots.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("#map-area").mapael({
+        map: {
+            name: "nigeria",
+            defaultArea: {
+                attrs: {
+                    fill: "#f4f4e8"
+                    , stroke: "#ced8d0"
+                }
+            }
+        }
+    });
+});
