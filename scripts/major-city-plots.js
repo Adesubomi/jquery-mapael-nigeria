23cd294325bfc1145e02f3a87ce8d394f073aa92
Oniya Daniel <daniel.prince84@gmail.com>
2018-01-07 23:58:48 +0100

    tested accuracy of coordinates and getCoords function

--- scripts/major-city-plots.js
+++ scripts/major-city-plots.js
@@ -8,6 +8,32 @@ $(function () {
                     , stroke: "#ced8d0"
                 }
             }
+        },
+        plots: {
+            'taraba': {
+                latitude: 8,
+                longitude: 10.5,
+                tooltip: {content: "Taraba State<br />"
+                    +"<small><strong>Capital:</strong> Jalingo</small>"}
+            },
+            'kwara': {
+                latitude: 8.5,
+                longitude: 5,
+                tooltip: {content: "Kwara State<br />"
+                    +"<small><strong>Capital:</strong> Ilorin</small>"}
+            },
+            'kano': {
+                latitude: 11.5,
+                longitude: 8.5,
+                tooltip: {content: "Kano State<br />"
+                    +"<small><strong>Capital:</strong> Kano</small>"}
+            },
+            'cross-river': {
+                latitude: 5.75,
+                longitude: 8.5,
+                tooltip: {content: "Cross-River State<br />"
+                    +"<small><strong>Capital:</strong> Port Harcourt</small>"}
+            }
         }
     });
 });
